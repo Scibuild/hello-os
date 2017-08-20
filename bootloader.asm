9d37bfeed6e112a779a3e29fff28ab9cee9d3728
Scibuild <zmbx100@gmail.com>
2017-08-20 19:30:09 +1000

    able to run code outside of bootsector

--- bootloader.asm
+++ bootloader.asm
@@ -1,5 +1,10 @@
 [org 0x7c00]
 
+mov     ax, cs
+mov     ds, ax
+mov     es, ax
+mov     ss, ax
+
 mov [BOOT_DRIVE], dl
 
 mov bp, 0x9000	;base pointer
@@ -11,29 +16,40 @@ mov cl, 2				; sector to read
 mov dl, [BOOT_DRIVE]
 call load_disk
 
-call print_string
-
-jmp $
+jmp bp
 
 load_disk:
-	push dx
+	;push dx
 	mov ah, 0x02	;The read instruction
 	mov al, 1			;Only read 1 sector
 	mov ch, 0x00  ;Cylinder 0
 	mov dh, 0x00	;Head 0
 	int 0x13
 	jc load_disk_error
-	pop dx				;interupt overwrote this so we are actually geting the first
+	;pop dx				;interupt overwrote this so we are actually geting the first
 								;character in memory
 	cmp al, 1
 	jne load_disk_error
+	ret
 load_disk_error:
-
-
+	mov bx, error_message
+	call print_string
 	jmp $
 
+error_message db "Error!", 0
+
+BOOT_DRIVE: db 0
+
+times 510 -($-$$) db 0
+dw 0xaa55
+;sector 2
+mov ax, 0x0120
+mov ds, ax
+mov bx, hello
+call print_string
+jmp $
 print_string:
-	pusha
+	push ax
 	mov ah, 0x0e
 print_character_loop:
 	mov al, [bx]
@@ -43,14 +59,6 @@ print_character_loop:
 	add bx, 1
 	jmp print_character_loop
 print_finished:
-	popa
+	pop ax
 	ret
-
-error_message db "Error!",0
-
-BOOT_DRIVE: db 0
-
-times 510 -($-$$) db 0
-dw 0xaa55
-;sector 2
-db "Hello world!",0
+hello db "Hello World!",10, 13, "How are you? Sorry you cant type yet.", 10, 13, 0
